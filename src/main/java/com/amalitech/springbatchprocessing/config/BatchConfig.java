359a93d4f6c56d0064600c67e559a655fbb7ba7c
richard-bouaro <richard.bouaro@amalitech.org>
2024-06-14 14:24:32 +0000

    chore(batch): Add TaskExecutor for Spring Batch processing
    - Configured SimpleAsyncTaskExecutor with a concurrency limit of 30.
    - Enabled asynchronous task execution to improve batch processing performance.
    - Updated Spring Batch configuration to include the custom task executor.

--- src/main/java/com/amalitech/springbatchprocessing/config/BatchConfig.java
+++ src/main/java/com/amalitech/springbatchprocessing/config/BatchConfig.java
@@ -19,69 +19,83 @@ import org.springframework.batch.item.json.JsonItemReader;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.core.io.FileSystemResource;
+import org.springframework.core.task.SimpleAsyncTaskExecutor;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.transaction.PlatformTransactionManager;
 
 @Configuration
 @RequiredArgsConstructor
 public class BatchConfig {
-    
-    private final UserEntityRepository userEntityRepository;
-    
-    private  final JobRepository jobRepository;
-    
-    private final PlatformTransactionManager platformTransactionManager;
-    
-    @Bean
-    public JsonItemReader<UserEntity> jsonItemReader(ObjectMapper objectMapper) {
-        JacksonJsonObjectReader<UserEntity> jsonObjectReader = new JacksonJsonObjectReader<>(UserEntity.class);
-        jsonObjectReader.setMapper(objectMapper);
-        
-        return new JsonItemReader<>(new FileSystemResource("src/main/resources/MOCK_DATA.json"), jsonObjectReader);
-    }
-    
-    @Bean
-    public UserProcessor processor() {
-        return new UserProcessor();
-    }
-    
-    @Bean
-    public RepositoryItemWriter<UserEntity> writer() {
-        RepositoryItemWriter<UserEntity> writer = new RepositoryItemWriter<>();
-        
-        writer.setRepository(userEntityRepository);
-        
-        writer.setMethodName("save");
-        
-        return writer;
-    }
-    
-    
-    
-    public Step importStep(){
-        ObjectMapper objectMapper = new ObjectMapper();
-        
-        // support Java 8 date time apis
-        objectMapper.registerModule(new JavaTimeModule());
-        
-        return new StepBuilder("jsonImport", jobRepository)
-               .<UserEntity, UserEntity>chunk(100, platformTransactionManager)
-               .reader(jsonItemReader(objectMapper))
-               .processor(processor())
-               .writer(writer())
-               .build();
-    }
-    
-    @Bean
-    public Job job(){
-        return new JobBuilder("jsonImportJob", jobRepository)
-               .start(importStep())
-               .build();
-    }
-    
-    
-    @Bean
-    TimedAspect timedAspect(MeterRegistry registry) {
-        return new TimedAspect(registry);
-    }
-    
+
+  private final UserEntityRepository userEntityRepository;
+
+  private final JobRepository jobRepository;
+
+  private final PlatformTransactionManager platformTransactionManager;
+
+  @Bean
+  public JsonItemReader<UserEntity> jsonItemReader(ObjectMapper objectMapper) {
+    JacksonJsonObjectReader<UserEntity> jsonObjectReader = new JacksonJsonObjectReader<>(
+        UserEntity.class);
+    jsonObjectReader.setMapper(objectMapper);
+
+    return new JsonItemReader<>(new FileSystemResource("src/main/resources/MOCK_DATA.json"),
+        jsonObjectReader);
+  }
+
+  @Bean
+  public UserProcessor processor() {
+    return new UserProcessor();
+  }
+
+  @Bean
+  public RepositoryItemWriter<UserEntity> writer() {
+    RepositoryItemWriter<UserEntity> writer = new RepositoryItemWriter<>();
+
+    writer.setRepository(userEntityRepository);
+
+    writer.setMethodName("save");
+
+    return writer;
+  }
+
+
+  public Step importStep() {
+    ObjectMapper objectMapper = new ObjectMapper();
+
+    // support Java 8 date time apis
+    objectMapper.registerModule(new JavaTimeModule());
+
+    return new StepBuilder("jsonImport", jobRepository)
+        .<UserEntity, UserEntity>chunk(100, platformTransactionManager)
+        .reader(jsonItemReader(objectMapper))
+        .processor(processor())
+        .writer(writer())
+//        .taskExecutor(taskExecutor())
+        .build();
+  }
+
+  @Bean
+  public Job job() {
+    return new JobBuilder("jsonImportJob", jobRepository)
+        .start(importStep())
+        .build();
+  }
+
+
+  @Bean
+  TimedAspect timedAspect(MeterRegistry registry) {
+    return new TimedAspect(registry);
+  }
+
+
+  public TaskExecutor taskExecutor() {
+    SimpleAsyncTaskExecutor asyncTaskExecutor = new SimpleAsyncTaskExecutor();
+
+    asyncTaskExecutor.setConcurrencyLimit(30);
+
+    return asyncTaskExecutor;
+  }
+
 }
